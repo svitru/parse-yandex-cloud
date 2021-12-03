c6bec8076637da76a1d937a9fdb8eb75392d6ce0
svitru <sv@sv-it.ru>
2021-12-03 16:19:06 +0200

    add parse pods in k8s

--- main.py
+++ main.py
@@ -2,14 +2,22 @@
 import argparse
 import json
 import logging
-
+import time
+import jwt
 import grpc
+
+import tempfile
+import os
+from kubernetes import client, config
 
 import yandexcloud
 from yandex.cloud.resourcemanager.v1.folder_service_pb2 import ListFoldersRequest
 from yandex.cloud.resourcemanager.v1.folder_service_pb2_grpc import FolderServiceStub
 from yandex.cloud.k8s.v1.cluster_service_pb2 import ListClustersRequest
 from yandex.cloud.k8s.v1.cluster_service_pb2_grpc import ClusterServiceStub
+from yandex.cloud.iam.v1.iam_token_service_pb2 import CreateIamTokenRequest
+from yandex.cloud.iam.v1.iam_token_service_pb2_grpc import IamTokenServiceStub
+
 
 def list_folder(sdk, cloud_id):
     folder_service = sdk.client(FolderServiceStub)
@@ -37,16 +45,44 @@ def list_cluster_k8s(sdk, folder_id):
             break
     return listOfClusters
 
+def get_token_k8s(sdk, service_account_json):
+    iam_service = sdk.client(IamTokenServiceStub)
+
+    now = int(time.time())
+    payload = {
+        'aud': 'https://iam.api.cloud.yandex.net/iam/v1/tokens',
+        'iss': service_account_json["service_account_id"],
+        'iat': now,
+        'exp': now + 360}
+    encoded_token = jwt.encode(
+        payload,
+        service_account_json["private_key"],
+        algorithm='PS256',
+        headers={'kid': service_account_json["id"]})
+    response = iam_service.Create(CreateIamTokenRequest(jwt=encoded_token))
+    return response
+
+def get_k8s_api(endpoint, certificate, iam_token):
+    aConfiguration = client.Configuration()
+    aConfiguration.host = endpoint
+    handler, nameFileCert = tempfile.mkstemp()
+    os.write(handler, str.encode(certificate))
+    os.close(handler)
+    aConfiguration.verify_ssl = True
+    aConfiguration.ssl_ca_cert = nameFileCert
+    aConfiguration.api_key = {"authorization": "Bearer " + iam_token}
+    aApiClient = client.ApiClient(aConfiguration)
+    v1 = client.CoreV1Api(aApiClient)
+    return v1
+
 def main():
     logging.basicConfig(level=logging.INFO)
     arguments = parse_args()
     interceptor = yandexcloud.RetryInterceptor(max_retry_count=5, retriable_codes=[grpc.StatusCode.UNAVAILABLE])
-    if arguments.token:
-        sdk = yandexcloud.SDK(interceptor=interceptor, token=arguments.token)
-    else:
-        with open(arguments.sa_json_path) as infile:
-            sdk = yandexcloud.SDK(interceptor=interceptor, service_account_key=json.load(infile))
-
+    with open(arguments.sa_json_path) as infile:
+        service_account_key=json.load(infile)
+        sdk = yandexcloud.SDK(interceptor=interceptor, service_account_key=service_account_key)
+    k8sToken = get_token_k8s(sdk, service_account_key)
     operation = list_folder(sdk, arguments.cloud_id)
 
     for folder in operation:
@@ -54,6 +90,13 @@ def main():
         listClusters = list_cluster_k8s(sdk, folder.id)
         for cluster in listClusters:
             print("  " + cluster.name)
+            print("    internal endpoint: " + cluster.master.endpoints.internal_v4_endpoint)
+            v1 = get_k8s_api(cluster.master.endpoints.internal_v4_endpoint, cluster.master.master_auth.cluster_ca_certificate, k8sToken.iam_token)
+            print("Listing pods with their IPs:")
+            ret = v1.list_pod_for_all_namespaces(watch=False)
+            for i in ret.items:
+                print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+            print("")
 
 
 def parse_args():
@@ -61,13 +104,11 @@ def parse_args():
         description=__doc__,
         formatter_class=argparse.RawTextHelpFormatter)
 
-    auth = parser.add_mutually_exclusive_group(required=True)
-    auth.add_argument(
+    parser.add_argument(
         '--sa-json-path',
         help='Path to the service account key JSON file.\nThis file can be created using YC CLI:\n'
-             'yc iam key create --output sa.json --service-account-id <id>',
+             'yc iam key create --output sa.json --service-account-id <id>', required=True
     )
-    auth.add_argument('--token', help='OAuth token')
     parser.add_argument('--cloud-id', help='Your Yandex.Cloud cloud id', required=True)
 
     return parser.parse_args()
